a53694fdc882055a259089a9a7ca1398d2c95981
Kevin Coleman <gabugabu@uw.edu>
2022-12-26 00:40:50 -0800

    updating

--- index.js
+++ index.js
@@ -1,35 +1,40 @@
-const copy = require('copy-dynamodb-table').copy
+/* eslint-disable sort-keys */
+/* eslint-disable no-inline-comments */
+const copy = require('copy-dynamodb-table').copy;
 
 const globalAWSConfig = { // AWS Configuration object http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Config.html#constructor-property
   accessKeyId: 'AKID',
   secretAccessKey: 'SECRET',
   region: 'eu-west-1'
+};
+
+const sourceAWSConfig = { region: 'us-west-2' };
+
+const destinationAWSConfig = { region: 'us-west-1' };
+
+function promiseCopy(data) {
+  return new Promise((resolve, reject) => {
+    copy(data, function (err, result) {
+      if (err) {
+        return reject(err);
+      }
+      resolve(result);
+    });
+  });
 }
 
-const sourceAWSConfig = {
-  region: 'us-west-2'
-}
-
-const destinationAWSConfig = {
-  region: 'us-east-1' // support cross zone copying
-}
-
-copy({
+(async () => {
+  await promiseCopy({
     config: globalAWSConfig,
     source: {
-      tableName: 'FosterStudent', // required
+      tableName: 'mbaPreRegRanked-prod', // required
       config: sourceAWSConfig // optional , leave blank to use globalAWSConfig
     },
     destination: {
-      tableName: 'FosterStudent', // required
+      tableName: 'mbaPreRegRanked-gabugabu', // required
       config: destinationAWSConfig // optional , leave blank to use globalAWSConfig
     },
-    log: true,// default false
-    create : false // create destination table if not exist
-  },
-  function (err, result) {
-    if (err) {
-      console.log(err)
-    }
-    console.log(result)
-  })
+    log: true, // default false
+    create: true // create destination table if not exist
+  });
+})();
